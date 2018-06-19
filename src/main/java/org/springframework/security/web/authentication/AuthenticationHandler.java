5434dcf3faccf8c49b37cd788c6616704f6dabd9
이광식(협력사) <u2waremanager@poscoict.com>
2018-06-19 11:11:13 +0900

    -

---
+++ src/main/java/org/springframework/security/web/authentication/AuthenticationHandler.java
@@ -0,0 +1,11 @@
+package org.springframework.security.web.authentication;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+public interface AuthenticationHandler extends AuthenticationSuccessHandler, AuthenticationFailureHandler, LogoutSuccessHandler, AccessDeniedHandler, AuthenticationEntryPoint {
+
+}
