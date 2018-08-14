db3a4eb90d2de1b4ac29b8c64c04317953eeeb5a
u2ware <u2waremanager@poscoict.com>
2018-08-14 15:49:27 +0900

    -

--- src/main/java/org/springframework/security/web/authentication/AuthenticationHandler.java
+++ src/main/java/org/springframework/security/web/authentication/AuthenticationHandler.java
@@ -11,4 +11,7 @@ public interface AuthenticationHandler
 				AccessDeniedHandler, 
 				AuthenticationEntryPoint {
 
+	public String[] getAllowedPaths();
+	public String[] getAuthenticatedPaths();
+
 }
